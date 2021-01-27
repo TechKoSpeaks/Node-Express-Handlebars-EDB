0fd4a4c9976ff6271e800d5f270f49d9db6fc5a4
Travis Ko <tkospeaks@gmail.com>
2021-01-26 16:49:25 -0800

    Added in delete function for orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,5 @@
 // Import MySQL connection.
 const connection = require("../config/connection.js");
-
 // Object for all our SQL statement functions.
 const orm = {
   selectAll: (tableInput, cb) => {
@@ -15,7 +14,6 @@ const orm = {
   insertOne: (table, newRowData, cb) => {
     const queryString = "INSERT INTO ?? SET ?";
     const values = [table, newRowData];
-
     connection.query(queryString, values, (err, result) => {
       if (err) {
         throw err;
@@ -23,12 +21,11 @@ const orm = {
       cb(result);
     });
   },
-
+  // Example of updateValues:
   // Example of condition: { id: 1 }
   updateOne: (table, updateValues, condition, cb) => {
-    const queryString = "UPDATE ?? SET ? WHERE ? LIMIT 1";
+    const queryString = "UPDATE ?? SET ? WHERE ?";
     const values = [table, updateValues, condition];
-
     console.log(queryString);
     connection.query(queryString, values, (err, result) => {
       if (err) {
@@ -37,8 +34,18 @@ const orm = {
       cb(result);
     });
   },
- 
+  // Add a `deleteOne` method which deletes row from a table which meets given
+  // condition parameter
+  deleteOne: (table, condition, cb) => {
+    const queryString = "DELETE FROM ?? WHERE ? LIMIT 1";
+    const values = [table, condition];
+    connection.query(queryString, values, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
 };
-
 // Export the orm object
 module.exports = orm;
